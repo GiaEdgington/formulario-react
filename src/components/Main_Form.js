9e7bd8c9bd96f3c7548e2858327b22454bc9dda9
Gia Edgington <gia.edgington@gmail.com>
2020-03-15 13:55:26 -0400

    Add state to main form

--- src/components/Main_Form.js
+++ src/components/Main_Form.js
@@ -1,6 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Step_One from './Step_One';
+import Step_Two from './Step_Two';
+import Step_Three from './Step_Three';
 
 export class Main_Form extends Component {
+
+    state = {
+        step: 1,
+        email: '',
+        password: '',
+        name: '',
+        age: ''
+    }
+
   render() {
     return (
       <div>
