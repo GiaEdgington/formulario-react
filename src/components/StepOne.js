0612fe08d77caf08a9bf1570a32b7bc0bf9b6256
Gia Edgington <gia.edgington@gmail.com>
2020-03-15 20:33:39 -0400

    Add style to form, inputs

--- src/components/StepOne.js
+++ src/components/StepOne.js
@@ -3,13 +3,20 @@ import React, { Component } from 'react';
 export class StepOne extends Component {
   render() {
     return (
-      <div className="flex-container">
-          <form>
+      <div className="container">
+          <form className="flex-container">
               <label>Email:</label>
-              <input type="email"></input><br/>
+              <input 
+              value=""
+              placeholder="your_email@email.com"
+              autocomplete="off"
+              type="email"></input><br/>
 
               <label>Password:</label>
-              <input type="password"></input><br/>
+              <input 
+              value=""
+              placeholder="your_password"
+              type="password"></input><br/>
           </form>
          {/* <button>Save and Continue</button> */}
       </div>
