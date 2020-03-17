948e03f2b6f6e7a3cc7fac13680b360bdaf65d2b
Gia Edgington <gia.edgington@gmail.com>
2020-03-16 20:08:42 -0400

    Implement SaveAndContinue functionality

--- src/components/StepOne.js
+++ src/components/StepOne.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 export class StepOne extends Component {
+
+  saveAndContinue = (event) => {
+    event.preventDefault();
+    this.props.nextStep();
+  }
+
   render() {
     return (
       <div className="container">
@@ -22,7 +28,7 @@ export class StepOne extends Component {
           </form>
           <div className="arrows">
             <svg xmlns="http://www.w3.org/2000/svg" opacity="0.2" width="24" height="24" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>
+            <svg onClick={this.saveAndContinue} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>
           </div>
       </div>
     )
